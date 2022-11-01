78d259ae6e5338f309c4e3b09687495c6b3ae91c
Tomo-ja <tomonao.hj@gmail.com>
2022-10-31 21:14:39 -0700

    update: change pagination styles

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -51,7 +51,7 @@ function Pagination({
           // Do not modify the aria-label below, it is used for Hatchways automation.
           aria-label="Goto previous page"
           onClick={onPrevious}
-          disabled={false} // change this line to disable a button.
+          disabled={currentPage === 1} // change this line to disable a button.
         >
           <ChevronLeftIcon />
         </button>
@@ -72,7 +72,7 @@ function Pagination({
           <li
             key={key}
             className="paginationItem"
-            aria-current="false" // change this line to highlight a current page.
+            aria-current={currentPage === pageNumber ? 'page' : 'false'} // change this line to highlight a current page.
           >
             <button
               type="button"
@@ -93,7 +93,7 @@ function Pagination({
           // Do not modify the aria-label below, it is used for Hatchways automation.
           aria-label="Goto next page"
           onClick={onNext}
-          disabled={false} // change this line to disable a button.
+          disabled={currentPage === paginationRange[paginationRange.length - 1]} // change this line to disable a button.
         >
           <ChevronRightIcon />
         </button>
