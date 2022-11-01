6020cacb602ca9dfad2101b4444e9ca99dabedee
Tomo-ja <tomonao.hj@gmail.com>
2022-10-31 19:01:15 -0700

    fix: update datas based on page and page size

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,22 +1,46 @@
 import BlogPost from "./BlogPost";
 import Pagination from "./Pagination";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import blogs from "../data/blogs.json";
 
 const PAGE_SIZES = [15, 25, 50, 100];
+const ALL_POSTS = blogs.posts
 
 function BlogList() {
-  const currentPaginationData = blogs.posts.slice(0, 15);
 
-  const updateRowsPerPage = () => {};
-  const updatePage = () => {};
+  const [currentPaginationData, setCurrentPaginationData] = useState(ALL_POSTS.slice(0, 15))
+  const [currentPage, setCurrentPage] = useState(1)
+  const [currentPageSize, setCurrentPageSize] = useState(15)
+
+  const updateRowsPerPage = (pageSize) => {
+    const numberedPageSize = parseInt(pageSize)
+    setCurrentPageSize(numberedPageSize)
+  };
+  const updatePage = (pageNumber) => {
+    setCurrentPage(pageNumber)
+  };
+
+  const updatePosts = () => {
+    const startPostIdx = (currentPage - 1) * currentPageSize
+    const endPostIdx = startPostIdx + currentPageSize
+    setCurrentPaginationData(ALL_POSTS.slice(startPostIdx, endPostIdx))
+  }
+
+  useEffect(() => {
+    updatePosts()
+  }, [currentPage])
+
+  useEffect(() => {
+    setCurrentPage(1)
+    updatePosts()
+  }, [currentPageSize])
 
   return (
     <div>
       <Pagination
-        currentPage={1}
+        currentPage={currentPage}
         totalCount={blogs.posts.length}
-        pageSize={15}
+        pageSize={currentPageSize}
         pageSizeOptions={PAGE_SIZES}
         onPageChange={updatePage}
         onPageSizeOptionChange={updateRowsPerPage}
--- src/hooks/usePagination.js
+++ src/hooks/usePagination.js
@@ -1,10 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { number } from "prop-types";
+
 export const DOTS = "...";
 
-function usePagination() {
-  /*
-    Rewrite the logic here to map out the pagination to be displayed.
-    
-  */
+function usePagination({currentPage, totalCount, pageSize}) {
+
+  // const [pagination, setPagination] = useState<[string | number]>([])
+
+  // const onPageSizeChanged = () 
+
+  // total count is number of all posts
+  // pageSize is how many post on one page
+  // current page is page number
   return [1, 2, 3, DOTS, 5];
 }
 
